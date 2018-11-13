ec5bf922f1bde27d17df73b9973fd560b4fae919
Vince Golveo <vince.golveo@gmail.com>
2018-11-13 14:15:14 -0500

    activate serviceworker and other fixes for lighthouse audit

--- src/js/restaurant.js
+++ src/js/restaurant.js
@@ -1,5 +1,5 @@
 import DBHelper from './dbhelper'
-// import './register'
+import './register'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css';
 
--- src/js/sw.js
+++ src/js/sw.js
@@ -1,39 +1,53 @@
 // think of this as a background server running
-var cacheID = 'mws-restaurant-v1';
+const cacheID = 'mws-restaurant-v1';
+const cacheSyncID = 'mws-restaurant-sync-v1'
 
 // install service worker caches
-self.addEventListener('install', event => {
-
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.bundle.js',
+        '/restaurant.html',
+        '/restaurant.bundle.js',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/index.bundle.js',
-          '/restaurant.html',
-          '/restaurant.bundle.js'
-        ]);
-      })
-    );
-  });
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-') && !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 
 // listen for fetch events
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(resp) {
-        return resp || fetch(event.request).then(function(response) {
-          let responseClone = response.clone();
-          caches.open('mws-restaurant-v1').then(function(cache) {
-            cache.put(event.request, responseClone);
-          });
-
-          return response;
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        let responseClone = response.clone();
+        caches.open(cacheID).then(function(cache) {
+          cache.put(event.request, responseClone);
         });
-      }).catch(function() {
-        return caches.match('/img/restaurant-default.jpg');
-      })
-    );
-  });
+        // console.log('Fetch successful: ', response);
+
+        return response;
+      });
+    }).catch(function(error) {
+      // return caches.match('/img/restaurant-default.jpg');
+      // console.log('Fetch responds with error: ', error);
+    })
+  );
+});
